ebb360d222885b97c374a75d80b7762c3519d710
kelvin <okorieebube1@gmail.com>
2021-12-01 07:21:15 +0100

    using props in react

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react"
 import Navbar from "./components/Navbar"
 import Hero from "./components/Hero.js"
+import Card from "./components/Card"
 /*
 Challenge: Build the Hero component.
 Check the Figma file for the design specifics.
@@ -11,6 +12,7 @@ export default function App() {
         <div>
             <Navbar />
             <Hero />
+            <Card/>
         </div>
     )
 }
---
+++ src/components/Card.js
@@ -0,0 +1,20 @@
+import React from "react"
+
+export default function Card() {
+
+
+    return (
+        <div className="card">
+            <img src="./images/katie-zaferes.png" className="card--image" />
+            <div className="card--stats">
+                <img src="./images/star.png" className="card--star" />
+                <span>5.0</span>
+                <span className="gray">(6) • </span>
+                <span className="gray">USA</span>
+            </div>
+            <p>Life Lessons with Katie Zaferes</p>
+            <p><span className="bold">From $136</span> / person</p>
+        </div>
+
+    )
+}
